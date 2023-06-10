61817015ecdf5f79530e2fcaf57a1d53e3f0e25b
Pavel Bozhek <themolodets2570@gmail.com>
2023-06-10 14:20:40 +0500

    Создание шаблона заготовки сайта

--- ToDoList/base/views.py
+++ ToDoList/base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Task
 
-
-def TaskList(request):
-    return HttpResponse('Список дел')
+class TaskList(ListView):
+    model = Task
