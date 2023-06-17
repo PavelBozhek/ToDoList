8507d68bbd7e7ef42cb03c6b29408a0d3a11f091
Pavel Bozhek <themolodets2570@gmail.com>
2023-06-17 21:17:00 +0500

    Ошибки устранены

--- ToDoList/base/views.py
+++ ToDoList/base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from .models import Task
@@ -19,5 +19,15 @@ class TaskDetail(DetailView):
 
 class TaskCreate(CreateView):
     model = Task
-    field = '__all__'
+    fields = ['title', 'description', 'complete']
+    success_url = reverse_lazy('task-create')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = ['title', 'description', 'complete']
+    success_url = reverse_lazy('task-update')
+
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name = 'task'
     success_url = reverse_lazy('tasks')
